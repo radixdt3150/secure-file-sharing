ab7cd66a34ed8252c446c4624147ea276021b3f0
radixdt3150 <mayur.upadhyay@radixweb.com>
2024-09-05 15:48:27 +0530

    chore: Adding few more test case for login

--- client/src/app/(authenticate)/login/login.test.tsx
+++ client/src/app/(authenticate)/login/login.test.tsx
@@ -37,6 +37,28 @@ describe('Login Page', (): void => {
         expect(submitBtn).toBeInTheDocument();
     });
 
+    test('Controls accepts input', async (): Promise<void> => {
+        // Data
+        const dummyEmail = 'john@doe.com', dummyPassword = "Password123";
+
+        // Arrange
+        render(<Page />);
+        const user = userEvent.setup(); // Initialize user-event
+        // Query elements
+        const email: HTMLInputElement = screen.getByTestId('email');
+        const password: HTMLInputElement = screen.getByTestId('password');
+
+        // Act
+        await user.type(email, dummyEmail);
+        await user.type(password, dummyPassword);
+
+        // Assert
+        expect(email.value).not.toBe('');
+        expect(email.value).toBe(dummyEmail);
+        expect(email.value).not.toBe('');
+        expect(password.value).toBe(dummyPassword);
+    })
+
     test('Displays error on submitting incomplete form', async () => {
         // Arrange
         render(<Page />);
@@ -57,6 +79,32 @@ describe('Login Page', (): void => {
             const passwordErr = screen.getByTestId('password-error');
             expect(emailErr).toBeInTheDocument();
             expect(passwordErr).toBeInTheDocument();
+        })
+    });
+
+    test('Has no errors on submitting a validated form', async (): Promise<void> => {
+        // Data
+        const validEmail = 'john@doe.com', validPassword = "Password123";
+
+        // Arrange
+        render(<Page />);
+        const user = userEvent.setup(); // Initialize user-event
+        // Query elements
+        const email: HTMLInputElement = screen.getByTestId('email');
+        const password: HTMLInputElement = screen.getByTestId('password');
+        const submitBtn = screen.getByTestId('submit-btn');
+
+        // Act
+        await user.type(email, validEmail);
+        await user.type(password, validPassword);
+        await user.click(submitBtn);
+
+        // Assert
+        await waitFor(() => {
+            const emailErr = screen.queryByTestId('email-error');
+            const passwordErr = screen.queryByTestId('password-error');
+            expect(emailErr).not.toBeInTheDocument();
+            expect(passwordErr).not.toBeInTheDocument();
         })
     })
 })
